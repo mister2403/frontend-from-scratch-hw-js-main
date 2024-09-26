1edca62ddb9978479cc3014300dc48bb4cd950d0
Denys Shevchuk <sko4436den@yahoo.com>
2024-09-26 02:16:08 +0200

    init JS lessons HW

---
+++ lesson-01/01.js
@@ -0,0 +1,13 @@
+let firstNumb = 0;
+let sum = 0;
+while ( firstNumb < 20) {
+firstNumb++;
+sum += firstNumb;
+} 
+console.log(sum);
+
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+
---
+++ lesson-01/03.js
@@ -0,0 +1,21 @@
+let currNumber = 0;
+let oddSumNumber = 0;
+
+while ( currNumber < 20 ) {
+
+    if (currNumber % 2 > 0){
+        oddSumNumber += currNumber;
+    }
+    
+    currNumber++;
+
+    };
+    console.log(oddSumNumber);
+
+/* Используйте цикл while для нахождения суммы всех нечётных чисел от 1 до 20.
+  Результат вычислений сохраните в переменной sum.
+
+  Вам понадобиться оператор остатка от деления %. Например, выражение 5 % 2 вернёт остаток 1.
+*/
+
+// your code
