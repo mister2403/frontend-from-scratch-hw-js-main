e5e90549644d5d2988548ed516f68dda56d9e678
Denys Shevchuk <sko4436den@yahoo.com>
2024-11-25 11:06:31 +0100

    ver.1 JS lessons-3 HW

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,12 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+function isEven(number) {
+    const check = number % 2
+    if (check === 0){
+        console.log('Число: ' + number + ' чётное ')
+    } else {
+        console.log('Число: ' + number + ' не чётное')
+    }
+}
+
--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,7 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(a, b, c) {
+    max = b > a ? b : a
+    max = max > c ? max : c
+    console.log(max);
+}
