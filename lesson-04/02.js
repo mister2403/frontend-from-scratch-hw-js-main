d70562925cb9d8fa46640b2b157cc06ba6fd2c58
Denys Shevchuk <sko4436den@yahoo.com>
2025-01-10 02:50:41 +0100

    ver.1 JS lessons-4 HW

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -15,4 +15,19 @@
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findUniqueElements() {}
+function findUniqueElements(array) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+      let isDuplicate = false;
+      for (let j = 0; j < result.length; j++) {
+        if (array[i] === result[j]) {
+          isDuplicate = true;
+          break;
+        }
+      }
+      if (!isDuplicate) {
+        result.push(array[i]);
+      }
+    }
+    return result;
+  }
